447d1ceb81759a577eba6e6cf307f9e21501748d
Alistair Gibb <aligibb_uk@hotmail.com>
2023-02-01 14:48:49 +0000

    Add spring data mongodb dependency

--- src/main/java/com/bezkoder.spring.data.mongodb/SpringBootDataMongodbApplication.java
+++ src/main/java/com/bezkoder.spring.data.mongodb/SpringBootDataMongodbApplication.java
@@ -1,4 +1,4 @@
-package com.springbootdatamongodb;
+package com.bezkoder.spring.data.mongodb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
