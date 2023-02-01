fad60f59330609fae646f4bec1f6d9063b3b1f5f
Alistair Gibb <aligibb_uk@hotmail.com>
2023-02-01 12:14:17 +0000

    Initial project setup

---
+++ src/main/java/com/bezkoder.spring.data.mongodb/SpringBootDataMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.springbootdatamongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataMongodbApplication.class, args);
+	}
+
+}
