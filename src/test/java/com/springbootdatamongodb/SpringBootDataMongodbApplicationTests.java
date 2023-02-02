3c8f16532cb34de29b523e7ff0fb962b76a45c84
Alistair Gibb <aligibb_uk@hotmail.com>
2023-02-02 09:42:02 +0000

    Update project files

--- src/test/java/com/springbootdatamongodb/SpringBootDataMongodbApplicationTests.java
+++ src/test/java/com/springbootdatamongodb/SpringBootDataMongodbApplicationTests.java
@@ -1,13 +1,13 @@
-package com.springbootdatamongodb;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SpringBootDataMongodbApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.springbootdatamongodb;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SpringBootDataMongodbApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
