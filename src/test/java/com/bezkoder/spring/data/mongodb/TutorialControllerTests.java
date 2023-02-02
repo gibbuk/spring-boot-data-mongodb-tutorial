87485bdb83a7131f9cb11aeeb480a1d67aa78886
Alistair Gibb <aligibb_uk@hotmail.com>
2023-02-02 17:03:47 +0000

    Update TutorialControllerTests.java - add tests

--- src/test/java/com/bezkoder/spring/data/mongodb/TutorialControllerTests.java
+++ src/test/java/com/bezkoder/spring/data/mongodb/TutorialControllerTests.java
@@ -54,11 +54,11 @@ public class TutorialControllerTests {
     @Test
     void shouldReturnTutorial() throws Exception{
         String id = "1L";
-        Tutorial tutorial = new Tutorial("Spring Boot @WebMVCTest", "Description", false);
+        Tutorial tutorial = new Tutorial(id,"Spring Boot @WebMVCTest", "Description", false);
 
         when(tutorialRepository.findById(id)).thenReturn(Optional.of(tutorial));
         mockMvc.perform(get("/api/tutorials/{id}", id)).andExpect(status().isOk())
-//                .andExpect(jsonPath("$.id").value(id))   //  Tutorial does not take id in constructor. Created by MongoDB. Therefore will return null
+                .andExpect(jsonPath("$.id").value(id))
                 .andExpect(jsonPath("$.title").value(tutorial.getTitle()))
                 .andExpect(jsonPath("$.description").value(tutorial.getDescription()))
                 .andExpect(jsonPath("$.published").value(tutorial.isPublished()))
@@ -114,6 +114,73 @@ public class TutorialControllerTests {
                 .andDo(print());
     }
 
+    @Test
+    void shouldReturnNoContentWhenFilter() throws Exception {
+        String title = "BezKoder";
+        MultiValueMap<String, String> paramsMap = new LinkedMultiValueMap<>();
+        paramsMap.add("title", title);
 
+        List<Tutorial> tutorials = Collections.emptyList();
+
+        when(tutorialRepository.findByTitleContaining(title)).thenReturn(tutorials);
+        mockMvc.perform(get("/api/tutorials").params(paramsMap))
+                .andExpect(status().isNoContent())
+                .andDo(print());
+    }
+    @Test
+    void shouldUpdateTutorial() throws Exception {
+        String id = "1L";
+
+        Tutorial tutorial = new Tutorial(id, "Spring Boot @WebMvcTest", "Description", false);
+        Tutorial updatedtutorial = new Tutorial(id, "Updated", "Updated", true);
+
+        when(tutorialRepository.findById(id)).thenReturn(Optional.of(tutorial));
+        when(tutorialRepository.save(any(Tutorial.class))).thenReturn(updatedtutorial);
+
+        mockMvc.perform(put("/api/tutorials/{id}", id).contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updatedtutorial)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value(updatedtutorial.getTitle()))
+                .andExpect(jsonPath("$.description").value(updatedtutorial.getDescription()))
+                .andExpect(jsonPath("$.published").value(updatedtutorial.isPublished()))
+                .andDo(print());
+    }
+
+    @Test
+    void shouldReturnNotFoundUpdateTutorial() throws Exception {
+        String id = "1L";
+
+        Tutorial updatedtutorial = new Tutorial(id, "Updated", "Updated", true);
+
+        when(tutorialRepository.findById(id)).thenReturn(Optional.empty());
+        when(tutorialRepository.save(any(Tutorial.class))).thenReturn(updatedtutorial);
+
+        mockMvc.perform(put("/api/tutorials/{id}", id).contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updatedtutorial)))
+                .andExpect(status().isNotFound())
+                .andDo(print());
+    }
+
+    @Test
+    void shouldDeleteTutorial() throws Exception {
+        String id = "1L";
+
+        doNothing().when(tutorialRepository).deleteById(id);
+        mockMvc.perform(delete("/api/tutorials/{id}", id))
+                .andExpect(status().isNoContent())
+                .andDo(print());
+    }
+
+    @Test
+    void shouldDeleteAllTutorials() throws Exception {
+        doNothing().when(tutorialRepository).deleteAll();
+        mockMvc.perform(delete("/api/tutorials"))
+                .andExpect(status().isNoContent())
+                .andDo(print());
+    }
 
 }
+
+
+
+
